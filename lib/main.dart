67594083286988b36a63e49f79b900a29672a17a
sannzay <sannzayreddy@gmail.com>
2025-10-30 19:24:40 -0400

    fix main dart file indentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,24 @@
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
 
-
 void main() {
-runApp(const SignupAdventureApp());
+  runApp(const SignupAdventureApp());
 }
-
 
 class SignupAdventureApp extends StatelessWidget {
-const SignupAdventureApp({super.key});
+  const SignupAdventureApp({super.key});
 
-
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'Signup Adventure',
-theme: ThemeData(
-primarySwatch: Colors.deepPurple,
-fontFamily: 'Roboto',
-useMaterial3: false,
-),
-home: const WelcomeScreen(),
-debugShowCheckedModeBanner: false,
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Signup Adventure',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: 'Roboto',
+        useMaterial3: false,
+      ),
+      home: const WelcomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
